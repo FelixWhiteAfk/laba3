1d448e4f32fb8b263c9f4689672a3f0ea23c6753
Valera <valeraevgenev@gmail.com>
2025-09-22 19:16:04 +0500

    Add new code with 8

--- file.py
+++ file.py
@@ -1,6 +1,10 @@
 for i in range(5):
-    print("Iteration:", i)    print("Iteration:", i)
+    print("Iteration:", i)
 count = 0
 while count < 5:
     print("Count:", count)
     count += 1
+count = 0
+while count < 8:
+    print("Count:", count)
+    count += 4
